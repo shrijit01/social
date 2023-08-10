8dae59a40767c7ae29319331d9861af5e360647a
Shrijit_01 <shrijitpandey113@gmail.com>
2023-08-10 16:00:58 +0530

    created two levels of check to submit the form in db id user is not loggedin

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,9 +3,10 @@ const Post = require('../models/post');
 
 
 
-
+/* POPULATE THE USER OF EACH POST  */
 module.exports.home = async function(req,res){
-    let foundPost = await Post.find();
+    let foundPost = await Post.find({})
+    .populate('user').exec();
     return res.render('home',{
         tittle:"Home",
         foundPost:foundPost
